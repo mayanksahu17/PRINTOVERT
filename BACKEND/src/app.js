658e99d900bcd5f09b016b6048fbe5cd56610843
Mayank <mayank0real0world@gmail.com>
2024-01-24 18:32:33 +0530

    hello

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -23,6 +23,10 @@ import userRouter from './routes/user.routes.js';
 import productRouter from './routes/product.routes.js'
 import adminRouter from './routes/admin.routes.js'
 // routes declairation 
+
+app.get("/",(req,res)=>{
+    res.json("hello")
+})
 app.use("/api/v1/users", userRouter)
 
 app.use("/api/v1/users/products", productRouter)
