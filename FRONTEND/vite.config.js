ea2ad740e71d6a9ec225250231e461cb147c75a2
Mayank <mayank0real0world@gmail.com>
2024-01-27 20:39:47 +0530

    ngrok url add

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000', // Make sure this path matches the backend API path
+      '/api': 'https://3wrmxn2x-8000.inc1.devtunnels.ms', // Make sure this path matches the backend API path
     },
   },
   plugins: [react()],
