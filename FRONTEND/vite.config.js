4fa02771e4eedd2ef336df13d30aca6c849c0442
Mayank <mayank0real0world@gmail.com>
2024-01-30 20:43:02 +0530

    change

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-  //   proxy: {
-  //     // '/api': 'https://3wrmxn2x-8000.inc1.devtunnels.ms', 
-  // },
+    proxy: {
+      '/api': 'https://3wrmxn2x-8000.inc1.devtunnels.ms', 
+  },
   plugins: [react()],
 }});
